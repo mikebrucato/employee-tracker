b2c325e25177025bae57ff08ff981e55fe5b294e
mikebrucato <brucatomike1@gmail.com>
2020-12-28 17:28:20 -0600

    add main tracker prompt

--- server.js
+++ server.js
@@ -1,22 +1,58 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var require 
+// Dependencies
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const cTable = require("console.table")
 
-var connection = mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
-
-  // Your port; if not 3306
   port: 3306,
-
-  // Your username
   user: "root",
-
-  // Your password
   password: "Password1",
   database: "employee_trackerDB"
 });
 
 connection.connect(function(err) {
   if (err) throw err;
-  runsearch()
+  mainMenuPrompt()
 })
+
+function mainMenuPrompt() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees?",
+        "View Employees but Role?",
+        "View Employees by Department?",
+        "Update Existing Employee",
+        "Add New Employee?",
+        "Add New Role?",
+        "Add New Department?"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "View All Employees?":
+        artistSearch();
+        break;
+
+      case "Find all artists who appear more than once":
+        multiSearch();
+        break;
+
+      case "Find data within a specific range":
+        rangeSearch();
+        break;
+
+      case "Search for a specific song":
+        songSearch();
+        break;
+
+      case "Find artists with a top song and top album in the same year":
+        songAndAlbumSearch();
+        break;
+      }
+    });
+}
